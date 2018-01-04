dcad83ff164985284e97ac884de0f5510cb76ad0
Tse-Hsien Chiang <jackblackevo@gmail.com>
2018-01-04 22:58:16 +0800

    Add Redux DevTools Extension code

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,10 @@ import Signin from './components/auth/signin'
 import reducers from './reducers'
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
-const store = createStoreWithMiddleware(reducers)
+const store = createStoreWithMiddleware(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
   <Provider store={store}>
