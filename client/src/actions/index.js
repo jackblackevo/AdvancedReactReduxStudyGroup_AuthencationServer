9991c86538953620fcd590fa3212087bb19cbebc
akari0624 <akari0624@hotmail.com>
2018-01-11 19:07:38 +0800

    programmatic navigation

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import {browserHistory} from 'react-router'
 
 const rootUrl = 'http://localhost:3090/'
 
@@ -6,7 +7,10 @@ export function signinUser ({ email, password }) {
   return (dispatch) => {
     axios.post(`${rootUrl}signin`, {email, password})
       .then(
-        res => console.log(res)
+        res => {
+          console.log(res)
+          browserHistory.push('/feature')
+        }
       )
       .catch(
         err => alert(err)
