2f3f32f8949140bade246258eff19c3966708e9e
akari0624 <akari0624@hotmail.com>
2018-01-11 19:07:38 +0800

    programmatic navigation

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,6 +2,7 @@
 export function signinUser ({ email, password }) {};
 =======
 import axios from 'axios'
+import {browserHistory} from 'react-router'
 import {AUTH_USER} from './type'
 
 const rootUrl = 'http://localhost:3090/'
@@ -10,7 +11,10 @@ export function signinUser ({ email, password }) {
   return (dispatch) => {
     axios.post(`${rootUrl}signin`, {email, password})   
       .then(
-        res => console.log(res)
+        res => {
+          console.log(res)
+          browserHistory.push('/feature')
+        }
       )       
       .catch(
         err => alert(err)
