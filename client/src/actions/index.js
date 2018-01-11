cf4d42b493c1e028a2428323f6dc61e12580b88d
akari0624 <akari0624@hotmail.com>
2018-01-11 18:55:29 +0800

    make signin request in server allow CORS setup ready

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,1 +1,15 @@
-export function signinUser ({ email, password }) {};
+import axios from 'axios'
+
+const rootUrl = 'http://localhost:3090/'
+
+export function signinUser ({ email, password }) {
+  return (dispatch) => {
+    axios.post(`${rootUrl}signin`, {email, password})
+      .then(
+        res => console.log(res)
+      )
+      .catch(
+        err => alert(err)
+      )
+  }
+};
