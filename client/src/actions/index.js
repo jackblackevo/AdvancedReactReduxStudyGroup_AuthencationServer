abb41eddf4cb9c8d8772fa543450c0b2d621578a
GillianY <jjjgrace01@gmail.com>
2018-01-11 15:52:05 +0800

    import redux thunk
    npm --save redux-thunk

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,1 +1,17 @@
-export function signinUser ({ email, password }) {};
+export function signinUser ({ email, password }) {
+    return function(dispatch){
+      //  dispatch({type:  });
+
+      //submit email/pwd to server
+
+      //if request good
+      //update state( auth) 
+      //save JWT token 
+      //redirect to route 'feature'
+
+      //if request bad 
+      //show error 
+    }
+};
+
+
