1c20c2890ba2aa23703cb669fd70bddad3bc108c
Tse-Hsien Chiang <jackblackevo@gmail.com>
2018-01-13 10:27:45 +0800

    Revert "Week12"

--- client/src/middleware/action_logger.js
+++
@@ -1,6 +0,0 @@
-export default function loggerMiddleware ({dispatch, getState}) {
-  return next => action => {
-    console.log('action logger :', action)
-    return next(action)
-  }
-}
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
 import { reducer as form } from 'redux-form'
-import auth from './authReducer'
 
 const rootReducer = combineReducers({
-  form,
-  auth
+  form
 })
 
 export default rootReducer
