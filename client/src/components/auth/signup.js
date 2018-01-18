ec76246b0174fd510be5d1ce6884b4cf599ed505
GillianY <jjjgrace01@gmail.com>
2018-01-18 14:52:57 +0800

    Implementing Validation Logic

--- client/src/components/auth/signup.js
+++ client/src/components/auth/signup.js
@@ -15,7 +15,9 @@ class Signup extends Component {
       <fieldset className="form-group">
         <label>Password:</label>
         <input className="form-control" {...password} type="password" />
-      </fieldset>
+        {password.touched && password.error && <div className="error">{password.error}</div>}
+
+       </fieldset>
       <fieldset className="form-group">
         <label>Confirm Password:</label>
         <input className="form-control" {...passwordConfirm} type="password" />
@@ -26,7 +28,18 @@ class Signup extends Component {
   }
 }
 
+function validate(formProps) {
+  const errors = {};
+
+  if (formProps.password !== formProps.passwordConfirm) {
+    errors.password = 'Passwords must match';
+  }
+
+  return errors;
+}
+
 export default reduxForm({
   form: 'signup',
-  fields: ['email', 'password', 'passwordConfirm']
+  fields: ['email', 'password', 'passwordConfirm'],
+  validate
 })(Signup);
