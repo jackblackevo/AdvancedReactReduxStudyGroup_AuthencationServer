830733a0f6bdb5cd38d6aa8aed403bbd7d3cc149
Tse-Hsien Chiang <jackblackevo@gmail.com>
2018-01-25 00:48:01 +0800

    113. Securing Individual Routes

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { browserHistory } from 'react-router'
 import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from './type'
 
 const rootUrl = 'http://localhost:3090'
@@ -10,6 +11,7 @@ export function signinUser ({ email, password }) {
         res => {
           dispatch({ type: AUTH_USER })
           localStorage.setItem('token', res.data.token)
+          browserHistory.push('/feature')
         }
       )
       .catch(() => {
@@ -30,6 +32,7 @@ export function signupUser ({ email, password }) {
       .then(response => {
         dispatch({ type: AUTH_USER })
         localStorage.setItem('token', response.data.token)
+        browserHistory.push('/feature')
       })
       .catch(error => {
         dispatch(authError(error.response.data.error))
--- client/src/components/header.js
+++ client/src/components/header.js
@@ -6,9 +6,14 @@ class Header extends Component {
   renderLinks () {
     if (this.props.authenticated) {
       // show a link to sign out
-      return <li className='nav-item'>
-        <Link className='nav-link' to='/signout'>Sign Out</Link>
-      </li>
+      return [
+        <li className='nav-item' key={1}>
+          <Link className='nav-link' to='/signout'>Sign Out</Link>
+        </li>,
+        <li className='nav-item' key={2}>
+          <Link className='nav-link' to='/feature'>Feature</Link>
+        </li>
+      ]
     } else {
       // show a link to sign in or sign up
       return [
